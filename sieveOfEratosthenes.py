b4042dcb658d0d155000071279eebf635f4e4f7c
ScruffyTheMoose <91182195+ScruffyTheMoose@users.noreply.github.com>
2022-01-26 17:13:54 -0600

    refactor sieve

--- sieveOfEratosthenes.py
+++ sieveOfEratosthenes.py
@@ -28,11 +28,32 @@ def sieve(n: int) -> list:
     result.sort() # python doesn't like converting a set to a list and sorting on the same line
     return result
 
-def toCSV(prime_list: list, list_name: str, file_name: str, ) -> pd.DataFrame:
-    """Takes list, converts list to DataFrame, saves to .csv file and returns the DataFrame."""
+def sieveToCSV(n: int, column_name: str, file_name: str) -> None:
+    """Sieve of Eratosthenes function. Give a value, n, and it will find all primes in range [1, n+1] and save the results to a CSV file."""
+    
+     # set for tracking composite numbers
+    composite_set = set()
+
+    # set for tracking prime numbers
+    prime_set = set()
+
+    # for all elements in the range n starting with first prime
+    for i in range(2, n+1):
+
+        # if i is not a composite number
+        if i not in composite_set:
+
+            # add i to the prime set
+            prime_set.add(i)
+
+            # for all elements from i^2 to n+1 stepping i values at a time
+            # by going i values each iteration, it adds only multiples of i to the composite set
+            # any values that it skips will by default be primes
+            for j in range(i*i, n+1, i):
+                composite_set.add(j)
+
+    result = list(prime_set)
+    result.sort()
     
     # writes list to dataframe with column labeled as list_name and saves to file_name
-    df = pd.DataFrame(prime_list, columns=[list_name]).to_csv(path_or_buf=f'{file_name}.csv')
-    
-    # returns the dataframe
-    return df
+    pd.DataFrame(result, columns=[column_name]).to_csv(path_or_buf=f'{file_name}.csv')
