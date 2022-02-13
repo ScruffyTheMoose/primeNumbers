54d31430dda6a000efeb9a004f6f7cd5c67d28d0
ScruffyTheMoose <91182195+ScruffyTheMoose@users.noreply.github.com>
2022-02-12 21:38:49 -0600

    build Germain Primes algo

---
+++ germain.py
@@ -0,0 +1,32 @@
+def run(primes: list) -> dict:
+    """
+    Returns a dict of all Germain Prime sequences identified in the given list
+    """
+
+    # dict for storing results
+    sequences = dict()
+
+    # building set of primes of O(1) checking
+    primeSet = set(primes)
+
+    # iterating through all primes in given list
+    for prime in primes:
+
+        # list for storing the current sequence achieved
+        seq = list()
+
+        # assigning the first prime to check as the current prime in the given list of primes
+        gt = prime
+
+        # checking that gt is Germain, and if so, adding to sequence and updating gt
+        while (gt * 2) + 1 in primeSet:
+            seq.append(gt)
+
+            gt = (gt * 2) + 1
+
+        # if the seq variable is not empty, meaning at least one Germain prime was identified, it gets added to results
+        if seq:
+            sequences[prime] = seq
+
+    # returning the results
+    return sequences
