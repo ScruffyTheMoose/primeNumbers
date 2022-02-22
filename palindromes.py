693b598f272c5669b0852714f1e33c8adf20f551
Jamie <ScruffyTheMoose@users.noreply.github.com>
2022-02-21 20:52:50 -0600

    var name

--- palindromes.py
+++ palindromes.py
@@ -1,31 +1,31 @@
-def run(number) -> bool:
+def run(prime: int) -> bool:
     """
-    Checks if the given number is a palindrome arithmetically.
+    Checks if the given prime is a palindrome arithmetically.
     Returns boolean.
     """
 
-    if number < 10:
+    if prime < 10:
         return True
 
-    # saving number to variable n to check if n == reverse
-    n = number
+    # saving prime to variable n to check if n == reverse
+    n = prime
 
-    # variable to track reversed number value
+    # variable to track reversed prime value
     rev = 0
 
-    # since we are using floor division for number, we iterate until number <= 0
-    while number > 0:
+    # since we are using floor division for prime, we iterate until prime <= 0
+    while prime > 0:
 
         # the currect digit being worked on is the remainder from mod 10, giving us the last digit
-        dig = number % 10
+        dig = prime % 10
 
         # multiply current reverse by 10 to allow for addition of dig
         rev = (rev * 10) + dig
 
-        # floor division on number to truncate last digit
-        number = number // 10
+        # floor division on prime to truncate last digit
+        prime = prime // 10
 
-    # if n, the starting number, is equal to the reverse then it is a palindrome
+    # if n, the starting prime, is equal to the reverse then it is a palindrome
     if n == rev:
         return True
     else:
